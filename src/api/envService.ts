b2177711b9826ed108d16bcbba2a60d5f44e985e
mirinfosys.simon <simon@mirinfosys.com>
2026-02-07 17:11:22 +0600

    #added vercel server fixing v5

--- src/api/envService.ts
+++ src/api/envService.ts
@@ -9,7 +9,7 @@ function getBackendApiBaseUrl(): string {
     if (process.env.REACT_APP_PUBLIC_PROJECT_MODE === 'development') {
         return process.env.REACT_APP_API_BASE_URL_DEVELOPMENT || 'http://localhost:3001';
     }
-    return process.env.NEXT_PUBLIC_VERCEL_API_BASE_URL || 'https://skysearch-two.vercel.app';
+    return process.env.NEXT_PUBLIC_VERCEL_API_BASE_URL || 'https://skysearch-kpu3.onrender.com'
 }
 
 /**
